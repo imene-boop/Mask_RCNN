36fffead469fa4eb07514b9a9feb2ab6bb732f1a
imene-boop <60331246+imene-boop@users.noreply.github.com>
2023-02-09 07:44:02 +0100

    Update parallel_model.py

--- mrcnn/parallel_model.py
+++ mrcnn/parallel_model.py
@@ -32,13 +32,11 @@ class ParallelModel(KM.Model):
         keras_model: The Keras model to parallelize
         gpu_count: Number of GPUs. Must be > 1
         """
-        super(ParallelModel, self).__init__()
         self.inner_model = keras_model
         self.gpu_count = gpu_count
         merged_outputs = self.make_parallel()
         super(ParallelModel, self).__init__(inputs=self.inner_model.inputs,
                                             outputs=merged_outputs)
-
     def __getattribute__(self, attrname):
         """Redirect loading and saving methods to the inner model. That's where
         the weights are stored."""
